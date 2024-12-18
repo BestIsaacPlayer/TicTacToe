8e363ae9c0753c56311b7991900607b30b5ae1e8
BestIsaacPlayer <milosz.plask@gmail.com>
2024-12-18 17:40:38 +0100

    Created base for the player controller and movement

--- Assets/Scripts/Board/Controller.cs
+++ Assets/Scripts/Board/Controller.cs
@@ -1,10 +1,36 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Board
 {
     public class Controller : MonoBehaviour
     {
         [field: SerializeField] public Cell.Controller[] Cells { get; private set; } = new Cell.Controller[9];
+        public int CurrentCellIndex { get; private set; } = 5;
+        public void MoveCurrentCell(Direction direction)
+        {
+            var x = CurrentCellIndex % 3;
+            Math.DivRem(CurrentCellIndex, 3, out var y);
+            switch (direction)
+            {
+                case Direction.Up:
+                    x = (x + 1) % 3;
+                    break;
+                case Direction.Down:
+                    x = (x - 1) % 3;
+                    break;
+                case Direction.Left:
+                    y = (y - 1) % 3;
+                    break;
+                case Direction.Right:
+                    y = (y + 1) % 3;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(direction), direction, null);
+            }
+
+            CurrentCellIndex = x + y;
+        }
 
         private void OnValidate()
         {
---
+++ Assets/Scripts/Player/Controller.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Player
+{
+    public class Controller : MonoBehaviour
+    {
+        [SerializeField] private ManagerParent managerParent;
+
+        private void OnValidate()
+        {
+            if (!managerParent) Debug.LogError($"The {Utility.Parser.FieldToName(nameof(managerParent))} field in the {gameObject.name} object is unset!");
+        }
+    }
+}
