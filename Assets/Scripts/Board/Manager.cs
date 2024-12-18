96d696df3c3f3b498b2b7c135198260b3a8b8009
BestIsaacPlayer <milosz.plask@gmail.com>
2024-12-18 23:11:50 +0100

    Implemented a board manager

---
+++ Assets/Scripts/Board/Manager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+namespace Board
+{
+    public class Manager : MonoBehaviour
+    {
+        [SerializeField] private Transform boardTransform;
+        [SerializeField] private Controller boardPrefab;
+        public Action ResetBoard;
+        private Controller _boardController;
+
+        private void OnValidate()
+        {
+            if (!boardTransform) Debug.LogError($"The {Utility.Parser.FieldToName(nameof(boardTransform))} field in the {gameObject.name} object is unset!");
+            if (!boardPrefab) Debug.LogError($"The {Utility.Parser.FieldToName(nameof(boardPrefab))} field in the {gameObject.name} object is unset!");
+        }
+
+        private void Awake()
+        {
+            ResetBoard += SpawnBoard;
+            ResetBoard?.Invoke();
+        }
+
+        private void SpawnBoard()
+        {
+            if (_boardController) Destroy(_boardController.gameObject);
+            _boardController = Instantiate(boardPrefab, boardTransform);
+        }
+    }
+}
--- Assets/Scripts/ManagerParent.cs
+++ Assets/Scripts/ManagerParent.cs
@@ -3,9 +3,11 @@
 public class ManagerParent : MonoBehaviour
 {
     [field: SerializeField] public Input.Manager InputManager { get; private set; }
+    [field: SerializeField] public Board.Manager BoardManager { get; private set; }
 
     private void OnValidate()
     {
         if (!InputManager) Debug.LogError($"The {Utility.Parser.FieldToName(nameof(InputManager))} field in the {gameObject.name} object is unset!");
+        if (!BoardManager) Debug.LogError($"The {Utility.Parser.FieldToName(nameof(BoardManager))} field in the {gameObject.name} object is unset!");
     }
 }
